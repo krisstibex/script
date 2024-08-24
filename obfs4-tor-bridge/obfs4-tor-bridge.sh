0e3338f9e6ad4ed51ffaa0db518cef69551dc516
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-08-25 06:37:46 +0800

    Update obfs4-tor-bridge.sh

--- obfs4-tor-bridge/obfs4-tor-bridge.sh
+++ obfs4-tor-bridge/obfs4-tor-bridge.sh
@@ -17,6 +17,7 @@ fi
 apt update
 apt install tor obfs4proxy -y
 sudo bash -c 'echo -e "Log notice file /var/log/tor/notices.log\nRunAsDaemon 1\nORPort auto\nExitpolicy reject *:*\nBridgeRelay 1\nServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy\nServerTransportListenAddr obfs4 [::]:8081\nExtORPort auto\nPublishServerDescriptor 0\nNickname ohmytor\nRelayBandwidthRate 50 MB\nRelayBandwidthBurst 200 MB" > /etc/tor/torrc'
+sed -i "s/ServerTransportListenAddr obfs4 \[::\]:[0-9]\+/ServerTransportListenAddr obfs4 [::]:$((RANDOM % 55536 + 10000))/" /etc/tor/torrc
 systemctl restart tor
 
 FINGERPRINT=$(sudo cat /var/lib/tor/fingerprint | awk '{print $2}')
