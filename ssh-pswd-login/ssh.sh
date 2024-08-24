ca47b265b1e0a93810f1ec1a098ea705f905f466
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-08-25 00:04:16 +0800

    Create ssh.sh

---
+++ ssh-pswd-login/ssh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service ssh restart && service sshd restart
+
+echo "Done."
