38dd6a5f60bf2c47b8c2f1879e650572696d3071
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-08-27 14:59:18 +0800

    Update snell.sh

--- snell/snell.sh
+++ snell/snell.sh
@@ -625,7 +625,7 @@ outputSnell() {
     echo -e "   ${BLUE}IPV6：${PLAIN} ${RED}${ipv6}${PLAIN}"
     echo -e "   ${BLUE}混淆(OBFS)：${PLAIN} ${RED}${obfs}${PLAIN}"
     echo -e "   ${BLUE}TCP记忆(TFO)：${PLAIN} ${RED}${tfo}${PLAIN}"
-    echo -e "   ${BLUE}dns地址(VER)：${PLAIN} ${RED}${dns}${PLAIN}"
+    echo -e "   ${BLUE}dns地址(DNS)：${PLAIN} ${RED}${dns}${PLAIN}"
     echo -e "   ${BLUE}Snell版本(VER)：${PLAIN} ${RED}${ver}${PLAIN}"
 }
 
