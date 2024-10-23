f78e038ed259d27e9609d519fefa6ceb04d234cb
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-10-23 16:26:40 +0800

    Update InstallJAVA.sh

--- install-jre-for-Minecraft/InstallJAVA.sh
+++ install-jre-for-Minecraft/InstallJAVA.sh
@@ -2,21 +2,18 @@
 
 cd /root
 echo "开始下载JAVA(Azul jdk)"
-wget https://cdn.azul.com/zulu/bin/zulu21.34.19-ca-jdk21.0.3-linux_x64.tar.gz
-wget https://cdn.azul.com/zulu/bin/zulu17.50.19-ca-jdk17.0.11-linux_x64.tar.gz
-wget https://cdn.azul.com/zulu/bin/zulu11.72.19-ca-jdk11.0.23-linux_x64.tar.gz
-wget https://cdn.azul.com/zulu/bin/zulu8.78.0.19-ca-jdk8.0.412-linux_x64.tar.gz
+wget https://cdn.azul.com/zulu/bin/zulu21.34.19-ca-jdk21.0.3-linux_x64.tar.gz -qO jdk21.tar.gz
+wget https://cdn.azul.com/zulu/bin/zulu17.50.19-ca-jdk17.0.11-linux_x64.tar.gz -qO jdk17.tar.gz
+wget https://cdn.azul.com/zulu/bin/zulu11.72.19-ca-jdk11.0.23-linux_x64.tar.gz -qO jdk11.tar.gz
+wget https://cdn.azul.com/zulu/bin/zulu8.78.0.19-ca-jdk8.0.412-linux_x64.tar.gz -qO jdk8.tar.gz
 
 echo "下载完成 开始解压"
-tar -xzvf zulu21.34.19-ca-jdk21.0.3-linux_x64.tar.gz > /dev/null 2>&1
-tar -xzvf zulu17.50.19-ca-jdk17.0.11-linux_x64.tar.gz > /dev/null 2>&1
-tar -xzvf zulu11.72.19-ca-jdk11.0.23-linux_x64.tar.gz > /dev/null 2>&1
-tar -xzvf zulu8.78.0.19-ca-jdk8.0.412-linux_x64.tar.gz > /dev/null 2>&1
+tar -xzf jdk{21,17,11,8}.tar.gz
 rm -f zulu*.gz
-mv zulu21.34.19-ca-jdk21.0.3-linux_x64 java21
-mv zulu17.50.19-ca-jdk17.0.11-linux_x64 java17
-mv zulu11.72.19-ca-jdk11.0.23-linux_x64 java11
-mv zulu8.78.0.19-ca-jdk8.0.412-linux_x64 java8
+mv zulu21.* java21
+mv zulu17.* java17
+mv zulu11.* java11
+mv zulu8.* java8
 
 echo "JAVA安装完成"
 ls -a
