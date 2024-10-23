391615f5ae16b3d9a11b56d71d8998671e889349
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-10-23 16:17:16 +0800

    Create sysctl.sh

---
+++ sysctl/sysctl.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# 一键优化 sysctl 设置并启用 BBR 和 CAKE
+
+# 网络性能优化
+sysctl -w net.core.rmem_max=16777216
+sysctl -w net.core.wmem_max=16777216
+sysctl -w net.core.rmem_default=262144
+sysctl -w net.core.wmem_default=262144
+sysctl -w net.ipv4.tcp_fastopen=3
+sysctl -w net.ipv4.tcp_window_scaling=1
+sysctl -w net.ipv4.tcp_rmem="4096 87380 16777216"
+sysctl -w net.ipv4.tcp_wmem="4096 65536 16777216"
+sysctl -w net.ipv4.tcp_syncookies=1
+
+# 启用 BBR 拥塞控制
+sysctl -w net.core.default_qdisc=fq_codel
+sysctl -w net.ipv4.tcp_congestion_control=bbr
+
+# 安全性优化
+sysctl -w net.ipv4.ip_forward=0
+sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
+sysctl -w net.ipv4.conf.all.accept_source_route=0
+sysctl -w net.ipv4.conf.default.accept_source_route=0
+
+# 内存和资源管理
+sysctl -w fs.file-max=2097152
+sysctl -w vm.swappiness=10
+sysctl -w fs.suid_dumpable=0
+sysctl -w kernel.shmmax=68719476736
+sysctl -w kernel.shmall=4294967296
+
+cat <<EOF >> /etc/sysctl.conf
+
+# 网络性能优化
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.core.rmem_default = 262144
+net.core.wmem_default = 262144
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 65536 16777216
+net.ipv4.tcp_syncookies = 1
+
+# 启用 BBR 拥塞控制
+net.core.default_qdisc = fq_codel
+net.ipv4.tcp_congestion_control = bbr
+
+# 安全性优化
+net.ipv4.ip_forward = 0
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+
+# 内存和资源管理
+fs.file-max = 2097152
+vm.swappiness = 10
+fs.suid_dumpable = 0
+kernel.shmmax = 68719476736
+kernel.shmall = 4294967296
+
+EOF
+
+# 启用 CAKE 队列管理
+tc qdisc replace dev eth0 root cake bandwidth 100mbit besteffort
+
+sysctl -p
