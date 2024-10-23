5abfa27cc87da1817b1da4ad09e93d8e263f84ce
Miku404 <82779172+krisstibex@users.noreply.github.com>
2024-10-23 16:30:13 +0800

    Update InstallJAVA.sh

--- install-jre-for-Minecraft/InstallJAVA.sh
+++ install-jre-for-Minecraft/InstallJAVA.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-cd /root
-echo "开始下载JAVA(Azul jdk)"
-wget https://cdn.azul.com/zulu/bin/zulu21.34.19-ca-jdk21.0.3-linux_x64.tar.gz -qO jdk21.tar.gz
-wget https://cdn.azul.com/zulu/bin/zulu17.50.19-ca-jdk17.0.11-linux_x64.tar.gz -qO jdk17.tar.gz
-wget https://cdn.azul.com/zulu/bin/zulu11.72.19-ca-jdk11.0.23-linux_x64.tar.gz -qO jdk11.tar.gz
-wget https://cdn.azul.com/zulu/bin/zulu8.78.0.19-ca-jdk8.0.412-linux_x64.tar.gz -qO jdk8.tar.gz
+INSTALL_DIR="/usr/lib/jvm"
+mkdir -p "$INSTALL_DIR"
 
-echo "下载完成 开始解压"
-tar -xzf jdk{21,17,11,8}.tar.gz
-rm -f zulu*.gz
-mv zulu21.* java21
-mv zulu17.* java17
-mv zulu11.* java11
-mv zulu8.* java8
+cd "$INSTALL_DIR" || { echo "无法切换到 $INSTALL_DIR 目录"; exit 1; }
 
-echo "JAVA安装完成"
-ls -a
-echo 您可以使用/root/java17/bin/java -jar server.jar 的方式启动您的服务器
+declare -A java_versions=(
+    [21]="https://cdn.azul.com/zulu/bin/zulu21.34.19-ca-jdk21.0.3-linux_x64.tar.gz"
+    [17]="https://cdn.azul.com/zulu/bin/zulu17.50.19-ca-jdk17.0.11-linux_x64.tar.gz"
+    [11]="https://cdn.azul.com/zulu/bin/zulu11.72.19-ca-jdk11.0.23-linux_x64.tar.gz"
+    [8]="https://cdn.azul.com/zulu/bin/zulu8.78.0.19-ca-jdk8.0.412-linux_x64.tar.gz"
+)
+
+for version in "${!java_versions[@]}"; do
+    wget "${java_versions[$version]}" -qO "jdk${version}.tar.gz"
+done
+
+for version in 21 17 11 8; do
+    tar -xzf "jdk${version}.tar.gz"
+    rm -f "jdk${version}.tar.gz"
+    mv "zulu${version}.*" "java${version}"
+done
+
+echo "JAVA 安装完成"
+ls -a "$INSTALL_DIR"
+echo "您可以使用 /usr/lib/jvm/java17/bin/java -jar server.jar 的方式启动您的服务器"
